3892b51fa014464783d75da2c708f7e1364d3983
Muzzy112 <g112ffe@gmail.com>
2022-06-16 17:44:07 +0300

    done

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,10 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="kot")
+    @Scope("prototype")
+    public Cat getCat(){
+        return new Cat();
+    }
 }
